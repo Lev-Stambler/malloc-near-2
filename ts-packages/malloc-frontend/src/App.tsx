105a2333514aafb2c32cbfec78457c51a2171680
Lev Stambler <levstamb@gmail.com>
2021-08-01 07:09:32 -0400

    gitignore

--- ts-packages/malloc-frontend/src/App.tsx
+++ ts-packages/malloc-frontend/src/App.tsx
@@ -14,50 +14,50 @@ export default function App() {
   const [loading, setLoading] = React.useState(false);
   const [explorerLink, setExplorerLink] = useState("");
 
-  // if not signed in, return early with sign-in prompt
-  if (!window.walletConnection.isSignedIn()) {
-    return (
-      <main>
-        <h1>Welcome to NEAR!</h1>
-        <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects to a
-          test network ("testnet") wallet. This works just like the main network
-          ("mainnet") wallet, but the NEAR Tokens on testnet aren't convertible
-          to other currencies – they're just for testing!
-        </p>
-        <p>Go ahead and click the button below to try it out:</p>
-        <p style={{ textAlign: "center", marginTop: "2.5em" }}>
-          <button onClick={login}>Sign in</button>
-        </p>
-      </main>
-    );
-  }
+  // // if not signed in, return early with sign-in prompt
+  // if (!window.walletConnection.isSignedIn()) {
+  //   return (
+  //     <main>
+  //       <h1>Welcome to NEAR!</h1>
+  //       <p>
+  //         To make use of the NEAR blockchain, you need to sign in. The button
+  //         below will sign you in using NEAR Wallet.
+  //       </p>
+  //       <p>
+  //         By default, when your app runs in "development" mode, it connects to a
+  //         test network ("testnet") wallet. This works just like the main network
+  //         ("mainnet") wallet, but the NEAR Tokens on testnet aren't convertible
+  //         to other currencies – they're just for testing!
+  //       </p>
+  //       <p>Go ahead and click the button below to try it out:</p>
+  //       <p style={{ textAlign: "center", marginTop: "2.5em" }}>
+  //         <button onClick={login}>Sign in</button>
+  //       </p>
+  //     </main>
+  //   );
+  // }
 
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    const txHash = url.searchParams.get("transactionHashes");
-    if (!txHash) return;
-    (async () => {
-      const result = await provider.txStatus(txHash, window.accountId);
-      console.log(result);
-      url.searchParams.delete("transactionHashes");
-      let newurl =
-        window.location.protocol +
-        "//" +
-        window.location.host +
-        window.location.pathname +
-        "?" +
-        url.searchParams.toString();
-      window.history.pushState({ path: newurl }, "", newurl);
+  // useEffect(() => {
+  //   const url = new URL(window.location.href);
+  //   const txHash = url.searchParams.get("transactionHashes");
+  //   if (!txHash) return;
+  //   (async () => {
+  //     const result = await provider.txStatus(txHash, window.accountId);
+  //     console.log(result);
+  //     url.searchParams.delete("transactionHashes");
+  //     let newurl =
+  //       window.location.protocol +
+  //       "//" +
+  //       window.location.host +
+  //       window.location.pathname +
+  //       "?" +
+  //       url.searchParams.toString();
+  //     window.history.pushState({ path: newurl }, "", newurl);
 
-      setExplorerLink(`${getConfig(env).explorerUrl}/transactions/${txHash}`);
-      setShowNotification(true);
-    })();
-  }, []);
+  //     setExplorerLink(`${getConfig(env).explorerUrl}/transactions/${txHash}`);
+  //     setShowNotification(true);
+  //   })();
+  // }, []);
 
 
 const defaultSplitter = `{
@@ -84,7 +84,7 @@ const defaultSplitter = `{
       </button>
       <main>
         <h1>Welcome {window.accountId}!</h1>
-        <form
+        {/* <form
           onSubmit={async (event) => {
             event.preventDefault();
 
@@ -129,7 +129,7 @@ const defaultSplitter = `{
               <button disabled={loading}>Execute the test!</button>
             </div>
           </fieldset>
-        </form>
+        </form> */}
       </main>
       {showNotification && <Notification explorerLink={explorerLink} />}
     </>
