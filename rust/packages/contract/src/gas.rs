9970d57a3f486203903cd88eb1505ab36f90208e
Lev Stambler <levstamb@gmail.com>
2021-08-18 12:51:00 -0400

    withdraw

--- rust/packages/contract/src/gas.rs
+++ rust/packages/contract/src/gas.rs
@@ -2,4 +2,4 @@ use near_sdk::Gas;
 
 // This can be brought down probs?
 pub const CALLBACK_GAS: Gas = 5_000_000_000_000 * 5;
-pub const CROSS_CONTRACT_BASE_GAS: Gas = 10_000_000_000_000;
+pub const CROSS_CONTRACT_BASE_GAS: Gas = 20_000_000_000_000;
